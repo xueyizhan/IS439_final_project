1c353b6da198e5db8f65befc94e6cc61238d5b45
薛饱 <xuebao@xuebaodeMacBook-Pro.local>
2023-05-04 19:20:59 -0500

    second commit

--- xue_yizhan_final_project/settings/base.py
+++ xue_yizhan_final_project/settings/base.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'xue_yizhan_final_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '../db.sqlite3',
     }
 }
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 LOGIN_REDIRECT_URL = 'about_urlpattern'
 LOGOUT_REDIRECT_URL = 'login_urlpattern'
